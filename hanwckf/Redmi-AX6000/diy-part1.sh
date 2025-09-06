3b1a84c8f612eb26ece219a705a096c031a2c213
huifukejian <xmw486@gmail.com>
2025-09-06 11:32:44 +0800

    Update diy-part1.sh

--- hanwckf/Redmi-AX6000/diy-part1.sh
+++ hanwckf/Redmi-AX6000/diy-part1.sh
@@ -27,4 +27,5 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+rm -rf ../feeds/luci/applications/luci-app-passwall/
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
